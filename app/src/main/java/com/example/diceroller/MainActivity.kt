4bff8d7fefdbb5eb1e79aa5ba5399257bb4f7666
Jacopo <jacopo.canton@gmail.com>
2021-11-26 16:14:29 +0100

    code cleanup

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -27,15 +27,26 @@ class MainActivity : AppCompatActivity() {
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
 
-        val luckyNumber = nrFaces
         when (diceRoll) {
-            luckyNumber -> Toast.makeText(this, "YOU GOT THE LUCKY NUMBER!", Toast.LENGTH_SHORT).show()
-            1 -> Toast.makeText(this, "So sorry! You rolled a 1. Try again!", Toast.LENGTH_SHORT).show()
-            2 -> Toast.makeText(this, "Sadly, you rolled a 2. Try again!", Toast.LENGTH_SHORT).show()
-            3 -> Toast.makeText(this, "Unfortunately, you rolled a 3. Try again!", Toast.LENGTH_SHORT).show()
-            4 -> Toast.makeText(this, "Don't cry! You rolled a 4. Try again!", Toast.LENGTH_SHORT).show()
-            5 -> Toast.makeText(this, "Apologies! You rolled a 5. Try again!", Toast.LENGTH_SHORT).show()
-            else -> Toast.makeText(this, "Crap! You rolled a ${diceRoll}. Try again!", Toast.LENGTH_SHORT).show()
+            nrFaces -> Toast.makeText(this, "YOU GOT THE LUCKY NUMBER!", Toast.LENGTH_SHORT).show()
+            1 -> Toast.makeText(this, "So sorry! You rolled a 1. Try again!", Toast.LENGTH_SHORT)
+                .show()
+            2 -> Toast.makeText(this, "Sadly, you rolled a 2. Try again!", Toast.LENGTH_SHORT)
+                .show()
+            3 -> Toast.makeText(
+                this,
+                "Unfortunately, you rolled a 3. Try again!",
+                Toast.LENGTH_SHORT
+            ).show()
+            4 -> Toast.makeText(this, "Don't cry! You rolled a 4. Try again!", Toast.LENGTH_SHORT)
+                .show()
+            5 -> Toast.makeText(this, "Apologies! You rolled a 5. Try again!", Toast.LENGTH_SHORT)
+                .show()
+            else -> Toast.makeText(
+                this,
+                "Crap! You rolled a ${diceRoll}. Try again!",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
