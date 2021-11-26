703b2306f1104276d0ccc0c8285accadcfebb2a6
Jacopo <jacopo.canton@gmail.com>
2021-11-26 16:22:10 +0100

    clear code to prepare for image

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -18,36 +18,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
 
-        val nrInput: EditText = findViewById(R.id.editTextNumberPassword)
-        val nrFacesText = nrInput.text.toString()
-        val nrFaces: Int = if (nrFacesText == "") 1 else Integer.parseInt(nrFacesText)
-
-        val dice = Dice(nrFaces)
+        val dice = Dice(6)
         val diceRoll = dice.roll()
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
-
-        when (diceRoll) {
-            nrFaces -> Toast.makeText(this, "YOU GOT THE LUCKY NUMBER!", Toast.LENGTH_SHORT).show()
-            1 -> Toast.makeText(this, "So sorry! You rolled a 1. Try again!", Toast.LENGTH_SHORT)
-                .show()
-            2 -> Toast.makeText(this, "Sadly, you rolled a 2. Try again!", Toast.LENGTH_SHORT)
-                .show()
-            3 -> Toast.makeText(
-                this,
-                "Unfortunately, you rolled a 3. Try again!",
-                Toast.LENGTH_SHORT
-            ).show()
-            4 -> Toast.makeText(this, "Don't cry! You rolled a 4. Try again!", Toast.LENGTH_SHORT)
-                .show()
-            5 -> Toast.makeText(this, "Apologies! You rolled a 5. Try again!", Toast.LENGTH_SHORT)
-                .show()
-            else -> Toast.makeText(
-                this,
-                "Crap! You rolled a ${diceRoll}. Try again!",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
     }
 }
 
